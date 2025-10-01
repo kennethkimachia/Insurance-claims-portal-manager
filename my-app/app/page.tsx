d1dd6f2f77db8117085b34ef5004d791c24b3382
kennethkimachia <114845663+kennethkimachia@users.noreply.github.com>
2025-10-01 10:07:18 +0300

    Removed unnecessary code

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,24 +1,6 @@
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
-import { ROUTES } from "@/lib/routes";
-
-
 export default async function Home() {
 
-  const {orgRole} = await auth()
-
-  switch (orgRole) {
-        case "org:admin":
-          redirect(ROUTES.ADMIN_DASHBOARD);
-          break;
-        case "org:agent":
-          redirect(ROUTES.AGENT_DASHBOARD);
-          break;
-        default:
-          redirect(ROUTES.USER_DASHBOARD);
-          break;
-      }
-
+  
   return (
 
     <div>
